2ed973dd19409609308fa798d9f8684c5dd7446b
sepro <sebastian.proost@gmail.com>
2021-01-10 16:53:23 +0100

    added CORS code

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import json
 
 
 def parse_scholar(request):
+    # Code to handle CORS (from docs)
+    if request.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     request_json = request.get_json()
     if request_json and 'user' in request_json:
         url = f'https://scholar.google.com/citations?user={request_json["user"]}'
@@ -16,4 +32,4 @@ def parse_scholar(request):
     hits = re.findall(r'<td class="gsc_rsb_std">(\d+)</td>', r.text)
     fields = ['citations', 'citations_recent', 'h_index', 'h_index_recent', 'i10_index', 'i10_index_recent']
 
-    return json.dumps(dict(zip(fields, hits)))
+    return (json.dumps(dict(zip(fields, hits))), 200, headers)
